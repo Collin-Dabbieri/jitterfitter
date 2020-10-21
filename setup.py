cff8edcc6e43aa771611f39833f114ea3d72930c
Collin Dabbieri <collin.m.dabbieri@vanderbilt.edu>
2020-10-21 12:42:30 -0500

    fixed license in setup

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Collin-Dabbieri/jitterfitter",
     packages=setuptools.find_packages(),
+    license_files = LICENSE,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
